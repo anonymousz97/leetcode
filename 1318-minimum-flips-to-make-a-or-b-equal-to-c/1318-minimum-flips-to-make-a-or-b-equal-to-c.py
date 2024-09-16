e654594e02d6964930871cd9ff4e47d02f1b293e
Minh Bui <33414340+anonymousz97@users.noreply.github.com>
2024-09-16 17:27:14 +0700

    Time: 28 ms (91.45%), Space: 16.5 MB (45.47%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.py
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -1,22 +1,4 @@
 import math
 class Solution:
     def minFlips(self, a: int, b: int, c: int) -> int:
-        max_n = int(math.log2(max(a, b, c))) + 1
-        rep_a = self.get_bin(a, max_n)
-        rep_b = self.get_bin(b, max_n)
-        rep_c = self.get_bin(c, max_n)
-        cnt = 0
-        for i in range(len(rep_c)-1, -1, -1):
-            if rep_c[i] == "0":
-                if rep_a[i] == "1":
-                    cnt += 1
-                if rep_b[i] == "1":
-                    cnt += 1
-            else:
-                if rep_a[i] == "0" and rep_b[i] == "0":
-                    cnt += 1
-        return cnt
-    
-    def get_bin(self, x, n=0):
-        return format(x, 'b').zfill(n)
-        
+        return ((c & a) ^ a).bit_count() + ((c & b) ^ b).bit_count() + (((a | b) & c) ^ c).bit_count()
