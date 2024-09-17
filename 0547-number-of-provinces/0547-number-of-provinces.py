349eaaf18ee706f3b4f54f4b3e94d86b35e3ed21
Minh Bui <33414340+anonymousz97@users.noreply.github.com>
2024-09-17 10:24:37 +0700

    Time: 179 ms (80.97%), Space: 17.7 MB (33.44%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,23 +1,19 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        lstVisit = [0] * len(isConnected)
-        cnt = 0
-        c_node = 0
-        while 0 in lstVisit:
-            lstVisit[c_node] = 1
-            queueVisit = []
-            queueVisit += [idx for idx, i in enumerate(isConnected[c_node]) if i == 1 and idx != c_node]
-            while queueVisit:
-                k = queueVisit.pop()
-                if not lstVisit[k]:
-                    queueVisit += [idx for idx, i in enumerate(isConnected[k]) if i == 1 and idx != k]
-                    lstVisit[k] = 1
-            cnt += 1
-            for idx, i in enumerate(lstVisit):
-                if i == 0:
-                    # print("assign next node")
-                    c_node = idx
-        return cnt
-                
-                
+
+        visited = [False] * len(isConnected)
+        count = 0
+
+        def dfs(u):
+            for v in range(len(isConnected)):
+                if isConnected[u][v] == 1 and visited[v] == False:
+                    visited[v] = True
+                    dfs(v)
         
+        for i in range(len(isConnected)):
+            if visited[i] == False:
+                count += 1
+                visited[i] == True
+                dfs(i)
+        
+        return count
