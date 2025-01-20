37d94eab9a7f0521362d69e25a6a3b26c3602ce2
Minh Bui <33414340+anonymousz97@users.noreply.github.com>
2025-01-20 10:28:09 +0700

    Time: 339 ms (5.36%), Space: 57.3 MB (5.05%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.py
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.py
@@ -1,32 +1,25 @@
 class Solution:
-    def firstCompleteIndex(self, arr, mat):
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+        tmp = []
+        dct_score = {}
+        map_pos = {}
         m, n = len(mat), len(mat[0])
-        N = m * n
-
-        num_to_index = [-1] * (N + 1)
-        for i, num in enumerate(arr):
-            num_to_index[num] = i
-
-        row_painted = [0] * m
-        col_painted = [0] * n
-
-        row_pos = [0] * N
-        col_pos = [0] * N
-        for i in range(m):
+        for j in range(n):
+            dct_score[f'col{j}'] = 0
+        for idx, i in enumerate(mat):
+            tmp += i
+            dct_score[f"row{idx}"] = sum(i)
             for j in range(n):
-                num = mat[i][j]
-                index_in_arr = num_to_index[num]
-                row_pos[index_in_arr] = i
-                col_pos[index_in_arr] = j
+                dct_score[f'col{j}'] += i[j]
+                map_pos[i[j]] = (idx, j)
 
-        for i, num in enumerate(arr):
-            row_idx = row_pos[num_to_index[num]]
-            col_idx = col_pos[num_to_index[num]]
+        for dix, i in enumerate(arr):
+            idx_r, idx_c = map_pos[i]
+            dct_score[f'row{idx_r}'] -= i
+            dct_score[f'col{idx_c}'] -= i
+            if dct_score[f'row{idx_r}'] == 0 or dct_score[f'col{idx_c}'] == 0:
+                return dix
 
-            row_painted[row_idx] += 1
-            col_painted[col_idx] += 1
 
-            if row_painted[row_idx] == n or col_painted[col_idx] == m:
-                return i
 
-        return -1
+        
