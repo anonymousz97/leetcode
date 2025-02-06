212de8a5a645ea8efcad82f0e8addea4cce4a3b9
Minh Bui <33414340+anonymousz97@users.noreply.github.com>
2025-02-06 10:09:07 +0700

    Time: 618 ms (14.69%), Space: 46.5 MB (22.27%) - LeetHub

---
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.py
@@ -0,0 +1,24 @@
+def n(x):
+    a = 1
+    for i in range(1,x+1, 1):
+        a *= i
+    return a
+
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        lst = {}
+        cnt = 0
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums), 1):
+                if nums[i] * nums[j] not in lst.keys():
+                    lst[nums[i] * nums[j]] = 1
+                else:
+                    lst[nums[i] * nums[j]] += 1
+        print(lst)
+        for i, j in lst.items():
+            if j > 1:
+                cnt += 8 * n(j)/(n(2)*n(j-2))
+        return int(cnt)
+
+
+        
