df1adefbf1a46611c99e205c6e00f4d316cbdfea
Minh Bui <33414340+anonymousz97@users.noreply.github.com>
2025-02-26 17:36:09 +0700

    Time: 5 ms (100%), Space: 29.4 MB (8.29%) - LeetHub

--- 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.py
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.py
@@ -1,14 +1,4 @@
 class Solution:
     def maxAbsoluteSum(self, nums: List[int]) -> int:
-        dp = [0] * len(nums)
-        dp[0] = abs(nums[0])
-        c_sum = (nums[0], nums[0])
-        max_value = abs(nums[0])
-        for idx, i in enumerate(nums[1:]):
-            dp[idx] = max(abs(c_sum[0]+i), abs(i), abs(c_sum[1]+i))
-            if dp[idx] > max_value: max_value = dp[idx]
-            c_sum = (max(c_sum[0]+i, i, c_sum[1]+i), min(c_sum[0]+i, i, c_sum[1]+i))
-            # print(i, max_value, c_sum)
-        return max_value
-
-        
+        s = list(accumulate(nums, initial=0))
+        return max(s) - min(s)
