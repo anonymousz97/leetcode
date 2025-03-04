0d156c0d24017760fed296df7ccc8390250e8a16
Minh Bui <33414340+anonymousz97@users.noreply.github.com>
2025-03-04 10:05:57 +0700

    Time: 0 ms (100%), Space: 17.8 MB (59.96%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,25 +1,9 @@
 class Solution:
-    def __init__(self):
-        self.lst = []
-        state = [0 for x in range(15)]
-        while True:
-            for i in range(len(state) - 1, -1, -1):
-                if state[i] == 0:
-                    state[i] = 1
-                    for j in range(i+1, len(state), 1):
-                        state[j] = 0
-                    break
-            num = 0
-            for i in range(len(state)):
-                num += 3 ** (len(state) - 1 - i) * state[i]
-            self.lst.append(num)
-            if sum(state) == len(state):
-                break
-            # if len(self.lst) == 10000:
-            #     break
-
     def checkPowersOfThree(self, n: int) -> bool:
-        if n in self.lst:
-            return True
-        return False
+        while n!=0:
+            rem = n%3
+            if rem==2:
+                return False
+            n = n//3
+        return True
         
