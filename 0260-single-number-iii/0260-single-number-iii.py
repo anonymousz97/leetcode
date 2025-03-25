741b9c9adc55039f273359117eecce8d7043ea14
Minh Bui <33414340+anonymousz97@users.noreply.github.com>
2025-03-25 15:06:43 +0700

    Time: 3 ms (67.99%), Space: 19.1 MB (70.12%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor2no = 0
+        for num in nums:
+            xor2no ^= num
+
+        lowestBit = xor2no & -xor2no
+        print(lowestBit)
+
+        result = [0, 0]
+        for num in nums:
+            if (lowestBit & num) == 0:
+                result[0] ^= num
+            else:
+                result[1] ^= num
+
+        return result
